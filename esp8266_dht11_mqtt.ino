024d66988e4c6c3582e31a86e6aa714a7328da77
Sander972 <alessandro.minca97@gmail.com>
2019-06-19 17:58:35 +0200

    refactor function to read data from sensor, reached limit for msg to publish

--- esp8266_dht11_mqtt.ino
+++ esp8266_dht11_mqtt.ino
@@ -5,6 +5,11 @@
 #include <PubSubClient.h>
 #include <WiFiUdp.h>
 #include <Wire.h>
+
+#include "Adafruit_BME680.h"
+#include <Adafruit_Sensor.h>
+#include <SPI.h>
+
 /*
 libs_links:
 https://github.com/knolleary/pubsubclient
@@ -12,16 +17,18 @@ https://github.com/bblanchon/ArduinoJson
 https://github.com/adafruit/DHT-sensor-library
 https://github.com/adafruit/Adafruit_Sensor
 https://github.com/arduino-libraries/NTPClient
+https://github.com/adafruit/Adafruit_BME680
 */
 
-#define DHTPIN_1 5    // what digital pin we're connected to NodeMCU (D1)
-#define DHTPIN_2 4    // D2
+#define DHTPIN_1 13   // D7
+#define DHTPIN_2 15   // D8
 #define DHTTYPE DHT22 // DHT 22
 
-#define wifi_ssid "NETGEAR52"
-#define wifi_password "niftylake235"
+#define wifi_ssid "sander"
+#define wifi_password "alessandrotuttomaiuscolo"
 
 #define mqtt_server "broker.hivemq.com"
+//#define mqtt_server "192.168.101.234"
 // string mqtt_user = "user"
 // string mqtt_password  = "password"
 
@@ -32,12 +39,15 @@ https://github.com/arduino-libraries/NTPClient
 #define telemetry "sander/camera"
 /*###topics###*/
 
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 DHT dht_1(DHTPIN_1, DHTTYPE);
 DHT dht_2(DHTPIN_2, DHTTYPE);
 WiFiClient espClient;
 PubSubClient client(espClient);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
+Adafruit_BME680 bme;
 
 void setup() {
   Serial.begin(115200);
@@ -47,6 +57,15 @@ void setup() {
   client.setServer(mqtt_server, 1883);
   timeClient.begin();
   timeClient.setTimeOffset(7200); // set timezone = GMT+2
+  pinMode(BUILTIN_LED, OUTPUT);
+
+  bme.begin(0x76);
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
 String macToStr(const uint8_t* mac) {
@@ -116,41 +135,58 @@ void reconnect() {
 
 String jsonComposer() {
 
-  timeClient.update();
+  // timeClient.update();
   String time = timeClient.getFormattedTime();
 
   float t1 = dht_1.readTemperature();
   float h1 = dht_1.readHumidity();
-  float hic1 = dht_1.computeHeatIndex(t1, h1, false);
+  // float hic1 = dht_1.computeHeatIndex(t1, h1, false);
 
   float t2 = dht_2.readTemperature();
   float h2 = dht_2.readHumidity();
-  float hic2 = dht_2.computeHeatIndex(t2, h2, false);
+  // float hic2 = dht_2.computeHeatIndex(t2, h2, false);
 
-  const int capacity = JSON_OBJECT_SIZE(10); // how much entries are in the json
+  bme.performReading();
+  float t3 = bme.temperature;
+  // float h3 = bme.humidity;
+  // float pre = (bme.pressure / 100.0);
+  // float gasr = (bme.gas_resistance / 1000.0);
+  // float alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  // String(msg).c_str()
+
+  const int capacity = JSON_OBJECT_SIZE(35); // how much entries are in the json
   StaticJsonDocument<capacity> msg;
 
   msg["device"].set(device);
   msg["time"].set(time);
 
-  if (isnan(t1) || isnan(t2)) {
-    msg["status"].set("Failed to read from DHT sensor!");
-    if (isnan(t1)) {
-      msg["status"].set("err sensor 1");
-    } else {
-      msg["status"].set("err sensor 2");
-    }
+  JsonObject dht22_1 = msg.createNestedObject("dht22_1");
+  if (isnan(t1)) {
+    dht22_1["status"].set("err sensor dht22_1");
   } else {
-
-    JsonObject dht22_1 = msg.createNestedObject("dht22_1");
-    dht22_1["humidity"].set(h1);
     dht22_1["temperature"].set(t1);
-    dht22_1["heat index"].set(hic1);
+    dht22_1["humidity"].set(h1);
+    dht22_1["heat index"].set(dht_1.computeHeatIndex(t1, h1, false));
+  }
 
-    JsonObject dht22_2 = msg.createNestedObject("dht22_2");
-    dht22_2["humidity"].set(h2);
+  JsonObject dht22_2 = msg.createNestedObject("dht22_2");
+  if (isnan(t2)) {
+    dht22_2["status"].set("err sensor dht22_2");
+  } else {
     dht22_2["temperature"].set(t2);
-    dht22_2["heat index"].set(hic2);
+    dht22_2["humidity"].set(h1);
+    dht22_2["heat index"].set(dht_2.computeHeatIndex(t2, h2, false));
+  }
+
+  JsonObject bme680 = msg.createNestedObject("bme680");
+  if (isnan(t3)) {
+    bme680["status"].set("err sensor VOC");
+  } else {
+    bme680["temperature"].set(t3);
+    bme680["humidity"].set(bme.humidity);
+    bme680["pressure"].set(bme.pressure / 100.0);
+    bme680["gas resistance"].set(bme.gas_resistance / 1000.0);
+    bme680["circa altitude"].set(bme.readAltitude(SEALEVELPRESSURE_HPA));
   }
 
   String json;
@@ -158,49 +194,33 @@ String jsonComposer() {
   return json;
 }
 
-/*
-float readDht(string sensor) {
-
-  float read[];
-
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  if (isnan(h) || isnan(t)) {
-    msg = "Failed to read from DHT sensor!";
-
-  } else {
-    float hic = dht.computeHeatIndex(t, h, false);
-
-    msg = ("Time: " + time + ", Humidity: " + String(h).c_str() +
-           "%, Temperature: " + String(t).c_str() +
-           "C, Heat index: " + String(hic).c_str());
-    // msg = ("Time: " + time + ", Humidity: " + h_s + ", Temperature: " + t_s
-    // + ", Heat index: " + hic_s);
-  }
-
-  return msg;
-}
-*/
-
 void loop() {
+
+  timeClient.update();
 
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-  // implement to prevent delay() if((Psec - Csec)>5){send msg}
-  // String msg = readDht();
-  // Serial.println(msg);
-  // client.publish(telemetry, String(msg).c_str(), true);
+  // implement to prevent delay() if((Psec - Csec)>5){send(msg)}
 
-  // String msg = "";
-  // serializeJson(jsonComposer(), msg);
-  // client.publish(telemetry, msg, true);
-
-  String msg = jsonComposer();
+  // String msg = jsonComposer();
+  String msg = jsonComposer().c_str();
   Serial.println(msg);
-  client.publish(telemetry, String(msg).c_str(), true);
 
-  delay(5000);
+  // int ok = client.publish(telemetry, String(msg).c_str(), true);
+  int ok = client.publish(telemetry, msg, true);
+
+  if (ok) {
+    // Serial.println(msg);
+    Serial.println("Publish ok");
+    // digitalWrite(BUILTIN_LED, HIGH);
+    // delay(200);
+    // digitalWrite(BUILTIN_LED, LOW);
+  } else {
+    Serial.println("err publishing");
+    digitalWrite(BUILTIN_LED, LOW);
+  }
+
+  delay(10000);
 }
